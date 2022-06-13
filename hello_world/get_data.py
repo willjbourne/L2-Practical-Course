da90d057edb6f002b969d22978081d63ad2b46af
willjbourne <willjbourne@gmail.com>
2022-06-13 12:26:11 +0100

    SRAM data now writing to text file & file structure formatting

--- hello_world/get_data.py
+++ hello_world/get_data.py
@@ -1,20 +1,27 @@
 import serial
 import numpy as np
 import time
+import struct
 # Collecting the data from the microbit
-serialPort = serial.Serial(port = "/dev/tty.usbmodem141102", baudrate=9600, bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE)
+serialPort = serial.Serial(port = "/dev/tty.usbmodem142102", baudrate=9600, bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE)
 
 d_list = []
 last_data = time.time()
-while(len(d_list) < 10): # (time.time()-last_data < 10) or
+while((time.time()-last_data < 10)): # (time.time()-last_data < 10) or
     # Wait until there is data waiting in the serial buffer
     if(serialPort.in_waiting > 0):
         last_data = time.time() # new data has arrived! update the time :)
         serialString = serialPort.readline() # Read data out of the buffer until a carraige return / new line is found
         d_list.append(serialString) # add serial data to the array
 
-print(d_list)
+temp = d_list[0][:-2] # extract the temperature from the serial stream
 
-with open("datafile.txt", 'wb') as f:
-    for item in d_list:
+
+
+with open("data/mb1/datafile-{0}-4.txt".format(temp), 'wb') as f:
+    for item in d_list[1:]:
         f.write(item[:-2])
+
+print(d_list)
+print(type(d_list[0]))
+print(len(d_list))
--- hello_world/hello_world.ino
+++ hello_world/hello_world.ino
@@ -1,27 +1,30 @@
 const int COL1 = 3;     // Column #1 control
 const int LED = 25;     // 'row 1' led
 
+
 void setup() {  
   Serial.begin(9600);
-  
-  Serial.println("microbit is ready!");
+  Serial.println("PUF is ready!");
 
-  // because the LEDs are multiplexed, we must ground the opposite side of the LED
-  pinMode(COL1, OUTPUT);
-  digitalWrite(COL1, LOW); 
-   
-  pinMode(LED, OUTPUT);   
+  // start temerature sensor
+  *(int*)(0x4000C000 + 0x0) = 0x1;
+  delay(5000);
+
+  // read DATAREADY
+  // Serial.println(*(int*)(0x4000C000 + 0x100));
+  // delay(5000);
 
   // print temperature in °C to serial
-  Serial.println(*(int*)(0x4000C000 + 0x508));
+  Serial.println(0.25 * *(int*)(0x4000C000 + 0x508));
+
 
   // start getting SRAM memory values
   int addy = 0x20000000;
   while (addy < 0x40000000) // upper limit of memory addresses 
   {
     Serial.println(*(char*)addy);
-    addy += 0x4;
-    delay(10);
+    addy += 0x1;
+    // delay(10);
   }
 }
 

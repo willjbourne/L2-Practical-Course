6baf19cb16300496207a339092301677327961a2
lveez <barneslouis225@gmail.com>
2022-06-14 11:50:11 +0100

    saves data to file in MATLAB

---
+++ get_data.m
@@ -0,0 +1,9 @@
+device = serialport("COM3", 115200);
+device.Timeout = 10;
+out = read(device, 999999, 'uint8');
+temp = out(1:4);
+temp = typecast(uint8(temp), 'int32');
+t = temp / 4;
+f = fopen("data/mb1/data1 -" +  t + ".bin", "w");
+fwrite(f, out(4:size(out,2)));
+fclose(f);

ed404e5b406304db16b494dc8cf5f8e63386a449
lveez <barneslouis225@gmail.com>
2022-06-14 12:38:10 +0100

    more data collected

--- get_data.m
+++ get_data.m
@@ -1,9 +1,9 @@
-device = serialport("COM3", 115200);
+device = serialport("COM4", 115200);
 device.Timeout = 10;
 out = read(device, 999999, 'uint8');
 temp = out(1:4);
 temp = typecast(uint8(temp), 'int32');
 t = temp / 4;
-f = fopen("data/mb3/data10 -" +  t + ".bin", "w");
+f = fopen("data/mb4/data10-" +  t + ".bin", "w");
 fwrite(f, out(5:size(out,2)));
 fclose(f);

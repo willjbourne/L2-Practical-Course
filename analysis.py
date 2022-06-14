a856dd9c5b8c6ae6987e4ee0cd8c297836920fab
willjbourne <willjbourne@gmail.com>
2022-06-14 14:42:29 +0100

    fixed combo hamming distances

--- analysis.py
+++ analysis.py
@@ -30,6 +30,15 @@ def flatten(xss):
     return [x for xs in xss for x in xs]
 
 
+def save(data, file):
+    with open(file, 'wb') as f:
+        np.save(f, data)
+
+def load(file):
+    with open(file, 'wb') as f:
+        return np.loaf(f)
+
+
 def read_data(filenames):
     byte_files = np.array([], dtype=bool)
     for file_link in data_files:
@@ -71,11 +80,12 @@ def hamming_distance_combinations(arr):
     # how many unique ways can we combine the datasets?
     combinations = [list(comb) for comb in itertools.combinations(range(len(arr[:,0])), 2)]
     for combination in combinations:
-        hamming_distances.append(hamming_distance(combination[0], combination[1]))
+        hamming_distances.append(hamming_distance(data[combination[0],:], data[combination[1],:]))
+
     return hamming_distances, combinations
 
-# ------
 
+## load datafiles into numpy array from files
 data_files = ["data/mb3/data1 -29.bin",
               "data/mb3/data2 -29.bin",
               "data/mb3/data3 -29.bin",
@@ -87,16 +97,19 @@ data_files = ["data/mb3/data1 -29.bin",
               "data/mb3/data9-29.bin",
               "data/mb3/data10-29.bin"
               ]
-
 data = read_data(data_files)
 
-# print(data)
+## get hamming distances between 2 specifc RAM datasets
+# ham_dist = hamming_distance(data[0,:], data[1,:])
+# print("Hamming count: {0}, Hamming fraction: {1}% difference".format(ham_dist, round(100*ham_dist/len(data[0]), 2)))
 
-ham_dist = hamming_distance(data[0,:], data[1,:])
-print("Hamming count: {0}, Hamming fraction: {1}%".format(ham_dist, round(100*ham_dist/len(data[0]), 2)))
 
+## get hamming distances between every possible RAM data pair
+hamming_distances, combinations = hamming_distance_combinations(data[:5, :])
+save(hamming_distances, "temp/hamming_distances.npy")
+save(combinations, "temp/combinations.npy")
+print(hamming_distances, combinations)
 
-# print(hamming_distance_combinations(data))
 
 
 
